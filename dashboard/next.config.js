919a516d6865daf5083fec3f4b986278def48dfe
Alaa ElGhoul <106868437+AE707@users.noreply.github.com>
2025-11-15 21:41:58 +0300

    Fix next.config.js - remove experimental.serverActions and sizes option

--- dashboard/next.config.js
+++ dashboard/next.config.js
@@ -11,7 +11,6 @@ const nextConfig = {
         hostname: '**',
       },
     ],
-    sizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
   },
   
   // Compress output
@@ -22,12 +21,6 @@ const nextConfig = {
   
   // React strict mode
   reactStrictMode: true,
-  
-  // Experimental features
-  experimental: {
-    // Enable React Server Components
-    serverActions: true,
-  },
   
   // Environment variables
   env: {
